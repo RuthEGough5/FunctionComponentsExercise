e55daaa28b0b898961337f68ceb52fe9679ccd22
Ruth Gough <ruthegough5@gmail.com>
2021-11-10 21:39:41 -0600

    Preparation for exercise begun with creation of index.html, app.js, and inclusion of assignment.txt.

---
+++ app.js
@@ -0,0 +1,8 @@
+//FUNCTION COMPONENTS EXERCISE 11.10.21
+
+//1A
+function GhostBusters() {
+    return `Who you gonna call?`
+};
+//1B
+React.DOM.render(<GhostBusters/>, document.getElementById(`root`));
